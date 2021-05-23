a9177319316dfe08691a8e62cef391e39ab5217d
AUTOGLASS\matheus.penido <matheus.penido@autoglass.com.br>
2021-05-23 18:05:34 -0300

    Comments to help learners.

--- DuckSimulator/Program.cs
+++ DuckSimulator/Program.cs
@@ -7,19 +7,33 @@ using DuckSimulator.Entities.MimicDevice.Specializations;
 namespace DuckSimulator
 {
     class Program
+
+    //For more details: https://www.amazon.com.br/gp/product/B08P3X99QP?pf_rd_r=0W3Q803Q1FZTF1JY7Y6Y&pf_rd_p=72a7651a-a7d8-4551-b248-c61480b6ce6e&pd_rd_r=23b19d1f-14d7-4cae-86dc-df0b7874c02c&pd_rd_w=tHqau&pd_rd_wg=meIfc&ref_=pd_gw_unk
     {
         static void Main(string[] args)
         {
+            //Create one kind of duck.
             DuckEntity mallard = new MallarDuck();
 
+            //Test entity
             mallard.PerformQuack();
             mallard.PerformFly();
 
+            //Create another kind of duck (another behavior duck)
             DuckEntity model = new ModelDuck();
-            model.PerformFly();
-            model.FlyBehavior = new FlyRocketPowered();
+
+            //Test new entity.
             model.PerformFly();
 
+            //Change duck behavior in runtime.
+            model.FlyBehavior = new FlyRocketPowered();
+
+            //Test new behavior.
+            model.PerformFly();
+
+            //Create another kind of Duck.
+            //It's not a child of DuckEntity class but at same time it can use the behaviors because there are no inhertitance here
+            //only composition from encapsulated algorithms behaviors regardless of DuckEntity. Great advantage!
             MimicDeviceEntity deviceForHunter = new DuckHunterDevice();
 
             deviceForHunter.PerformQuack();
